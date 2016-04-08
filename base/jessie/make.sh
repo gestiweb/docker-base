101a12513ef677b296fc5a79b6eb4e698aba0d98
David Martinez Marti <deavidsedice@gmail.com>
2016-04-08 16:03:29 +0200

    various changes

--- base/jessie/make.sh
+++ base/jessie/make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "WARN: Requires root or sudo to run."
 echo "WARN: Requires debootstrap to run."
-/usr/share/docker-engine/contrib/mkimage.sh -t gestiweb/minimal:debian-jessie debootstrap --variant=minbase jessie
+/usr/share/docker-engine/contrib/mkimage.sh -t gestiweb/base:debian-jessie debootstrap --variant=minbase jessie
